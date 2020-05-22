b3756a176a8a4ff69cb09857e6745083fa794991
hiron <contact@hrntsm.com>
2020-05-22 23:16:48 +0900

    [add] 全部材の断面の文字の図化出力追加

--- StbHopper/Component/SectionTag/Stb2GirderSec.cs
+++ StbHopper/Component/SectionTag/Stb2GirderSec.cs
@@ -11,27 +11,23 @@ using StbHopper.Util;
 
 namespace StbHopper.Component.SectionTag
 {
-    public class Stb2ColSec:GH_Component
+    public class Stb2GirderSec:GH_Component
     {
         private string _path;
         private int _size;
 
         private static StbNodes _nodes;
-        private static StbColumns _columns;
-        private static StbPosts _posts;
         private static StbGirders _girders;
-        private static StbBeams _beams;
-        private static StbBraces _braces;
         
-        public static StbSecColRC SecColumnRc;
-        public static StbSecBeamRC SecBeamRc;
-        public static StbSecColumnS SecColumnS;
-        public static StbSecBeamS SecBeamS;
-        public static StbSecBraceS SecBraceS;
-        public static StbSecSteel StbSecSteel;
+        private static StbSecColRC _secColumnRc;
+        private static StbSecBeamRC _secBeamRc;
+        private static StbSecColumnS _secColumnS;
+        private static StbSecBeamS _secBeamS;
+        private static StbSecBraceS _secBraceS;
+        private static StbSecSteel _stbSecSteel;
 
-        private readonly List<GH_Structure<GH_String>> _frameTags = new List<GH_Structure<GH_String>>();
-        private readonly List<List<Point3d>>_tagPos = new List<List<Point3d>>();
+        private GH_Structure<GH_String> _frameTags = new GH_Structure<GH_String>();
+        private List<Point3d> _tagPos = new List<Point3d>();
         
         /// <summary>
         /// Each implementation of GH_Component must provide a public 
@@ -40,8 +36,8 @@ namespace StbHopper.Component.SectionTag
         /// Subcategory the panel. If you use non-existing tab or panel names, 
         /// new tabs/panels will automatically be created.
         /// </summary>
-        public Stb2ColSec()
-          : base("Stb to SectionTag", "S2Sec", "Read ST-Bridge file and display", "StbHopper", "Section")
+        public Stb2GirderSec()
+          : base("StbtoGirderSectionTag", "S2GrSec", "Read ST-Bridge file and display Girder Section Tag", "StbHopper", "Section")
         {
         }
 
@@ -68,11 +64,7 @@ namespace StbHopper.Component.SectionTag
         /// </summary>
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
-            pManager.AddTextParameter("Columns", "Col", "output StbColumns to Brep", GH_ParamAccess.tree);
-            pManager.AddTextParameter("Girders", "Gird", "output StbGirders to Brep", GH_ParamAccess.tree);
-            pManager.AddTextParameter("Posts", "Pst", "output StbPosts to Brep", GH_ParamAccess.tree);
-            pManager.AddTextParameter("Beams", "Bm", "output StbBeams to Brep", GH_ParamAccess.tree);
-            pManager.AddTextParameter("Braces", "Brc", "output StbBraces to Brep", GH_ParamAccess.tree);
+            pManager.AddTextParameter("Girders", "Gr", "output StbGirders to Section Tag", GH_ParamAccess.tree);
         }
 
         /// <summary>
@@ -91,25 +83,22 @@ namespace StbHopper.Component.SectionTag
             Load(xDocument);
 
             // meshの生成
-            MakeGetTag();
+            GetTag();
 
-            for (var i = 0; i < 5; i++)
-            {
-                DA.SetDataTree(i, _frameTags[i]);
-            }
+            DA.SetDataTree(0, _frameTags);
         }
 
         public override void DrawViewportWires(IGH_PreviewArgs args)
         {
-            var column = _frameTags[0];
-            var columnPos = _tagPos[0];
-            var test = column.PathCount;
-            for (int i = 0; i < column.PathCount; i++)
+            if (_frameTags.DataCount != 0)
             {
-                var tags = column.Branches[i];
-                string tag = tags[0].ToString() + "\n" + tags[1].ToString() + "\n" + tags[2].ToString() + "\n" + 
-                             tags[3].ToString() + "\n" + tags[4].ToString() + "\n" + tags[5].ToString();
-                args.Display.Draw2dText(tag, Color.Black, columnPos[i], true, _size);
+                for (int i = 0; i < _frameTags.PathCount; i++)
+                {
+                    var tags = _frameTags.Branches[i];
+                    string tag = tags[0].ToString() + "\n" + tags[1].ToString() + "\n" + tags[2].ToString() + "\n" + 
+                                 tags[3].ToString() + "\n" + tags[4].ToString() + "\n" + tags[5].ToString();
+                    args.Display.Draw2dText(tag, Color.Black, _tagPos[i], false, _size);
+                }
             }
         }
 
@@ -124,31 +113,26 @@ namespace StbHopper.Component.SectionTag
         /// It is vital this Guid doesn't change otherwise old ghx files 
         /// that use the old ID will partially fail during loading.
         /// </summary>
-        public override Guid ComponentGuid => new Guid("328D76C1-2295-48A0-A359-F7B4B556FC4B");
+        public override Guid ComponentGuid => new Guid("D72C9B9D-6233-44EF-B588-D2854BB4FB4F");
 
         private static void Init()
         {
             _nodes = new StbNodes();
-            _columns = new StbColumns();
-            _posts = new StbPosts();
             _girders = new StbGirders();
-            _beams = new StbBeams();
-            _braces = new StbBraces();
-            SecColumnRc = new StbSecColRC();
-            SecBeamRc = new StbSecBeamRC();
-            SecColumnS = new StbSecColumnS();
-            SecBeamS = new StbSecBeamS();
-            SecBraceS = new StbSecBraceS();
-            StbSecSteel = new StbSecSteel();
+            _secColumnRc = new StbSecColRC();
+            _secBeamRc = new StbSecBeamRC();
+            _secColumnS = new StbSecColumnS();
+            _secBeamS = new StbSecBeamS();
+            _secBraceS = new StbSecBraceS();
+            _stbSecSteel = new StbSecSteel();
         }
 
         private static void Load(XDocument xDoc)
         {
             var members = new List<StbData>()
             {
-                _nodes,
-                _columns, _posts, _girders, _beams, _braces,
-                SecColumnRc, SecColumnS, SecBeamRc, SecBeamS, SecBraceS, StbSecSteel
+                _nodes, _girders,
+                _secColumnRc, _secColumnS, _secBeamRc, _secBeamS, _secBraceS, _stbSecSteel
             };
 
             foreach (var member in members)
@@ -157,18 +141,11 @@ namespace StbHopper.Component.SectionTag
             }
         }
 
-        private void MakeGetTag()
+        private void GetTag()
         {
-            var stbFrames = new List<StbFrame>() {
-                _columns, _girders, _posts, _beams, _braces
-            };
-            
             var tags = new CreateTag(_nodes);
-            foreach (var frame in stbFrames)
-            {
-                _frameTags.Add(tags.Frame(frame));
-                _tagPos.Add(tags.TagPos);
-            }
+            _frameTags = tags.Frame(_girders, _secColumnRc, _secColumnS, _secBeamRc, _secBeamS, _secBraceS, _stbSecSteel);
+            _tagPos = tags.TagPos;
         }
     }
 }
--- StbHopper/Util/CreateTag.cs
+++ StbHopper/Util/CreateTag.cs
@@ -19,32 +19,30 @@ namespace StbHopper.Util
             this._nodes = nodes;
         }
 
-        public GH_Structure<GH_String> Frame(StbFrame frame)
+        public GH_Structure<GH_String> Frame(StbFrame frame, StbSecColRC colRc, StbSecColumnS colS, StbSecBeamRC beamRc, StbSecBeamS beamS, StbSecBraceS braceS, StbSecSteel secSteel)
         {
             GH_Structure<GH_String> sec = new GH_Structure<GH_String>();
-
+        
             double p1 = 0;
             double p2 = 0;
             double p3 = 0;
             double p4 = 0;
             string shape = string.Empty;
             string name = string.Empty;
-
+        
             for (int eNum = 0; eNum < frame.Id.Count; eNum++)
             {
                 int idSection = frame.IdSection[eNum];
                 KindsStructure kind = frame.KindStructure[eNum];
                 GH_Path ghPath = new GH_Path(new int[]{eNum});
-
+        
                 // 始点と終点の座標取得
-                int nodeIndexStart = _nodes.Id.IndexOf(frame.IdNodeStart[eNum]);
-                int nodeIndexEnd = _nodes.Id.IndexOf(frame.IdNodeEnd[eNum]);
-                Point3d nodeStart = new Point3d(_nodes.X[nodeIndexStart], _nodes.Y[nodeIndexStart], _nodes.Z[nodeIndexStart]);
-                Point3d nodeEnd = new Point3d(_nodes.X[nodeIndexEnd], _nodes.Y[nodeIndexEnd], _nodes.Z[nodeIndexEnd]);
-                TagPos.Add(new Point3d((nodeStart.X + nodeEnd.X) / 2.0,
-                    (nodeStart.Y + nodeEnd.Y) / 2.0,
-                    (nodeStart.Z + nodeEnd.Z) / 2.0));
-
+                int startIndex = _nodes.Id.IndexOf(frame.IdNodeStart[eNum]);
+                int endIndex = _nodes.Id.IndexOf(frame.IdNodeEnd[eNum]);
+                Point3d nodeStart = new Point3d(_nodes.X[startIndex], _nodes.Y[startIndex], _nodes.Z[startIndex]);
+                Point3d nodeEnd = new Point3d(_nodes.X[endIndex], _nodes.Y[endIndex], _nodes.Z[endIndex]);
+                TagPos.Add(new Point3d((nodeStart.X + nodeEnd.X) / 2.0, (nodeStart.Y + nodeEnd.Y) / 2.0, (nodeStart.Z + nodeEnd.Z) / 2.0));
+        
                 int secIndex;
                 ShapeTypes shapeType = ShapeTypes.BOX;
                 switch (kind)
@@ -54,22 +52,22 @@ namespace StbHopper.Util
                         {
                             case FrameType.Column:
                             case FrameType.Post:
-                                secIndex = Stb2ColSec.SecColumnRc.Id.IndexOf(idSection);
-                                name = Stb2ColSec.SecColumnRc.Name[secIndex];
-                                p1 = Stb2ColSec.SecColumnRc.Height[secIndex];
-                                p2 = Stb2ColSec.SecColumnRc.Width[secIndex];
+                                secIndex = colRc.Id.IndexOf(idSection);
+                                name = colRc.Name[secIndex];
+                                p1 = colRc.Height[secIndex];
+                                p2 = colRc.Width[secIndex];
                                 break;
                             case FrameType.Girder:
                             case FrameType.Beam:
-                                secIndex = Stb2ColSec.SecBeamRc.Id.IndexOf(idSection);
-                                name = Stb2ColSec.SecBeamRc.Name[secIndex];
-                                p1 = Stb2ColSec.SecBeamRc.Depth[secIndex];
-                                p2 = Stb2ColSec.SecBeamRc.Width[secIndex];
+                                secIndex = beamRc.Id.IndexOf(idSection);
+                                name = beamRc.Name[secIndex];
+                                p1 = beamRc.Depth[secIndex];
+                                p2 = beamRc.Width[secIndex];
                                 p3 = 0;
                                 p4 = 0;
                                 break;
                         }
-
+        
                         shapeType = p1 <= 0 ? ShapeTypes.Pipe : ShapeTypes.BOX;
                         break;
                     case KindsStructure.S:
@@ -79,27 +77,27 @@ namespace StbHopper.Util
                         {
                             case FrameType.Column:
                             case FrameType.Post:
-                                idShape = Stb2ColSec.SecColumnS.Id.IndexOf(idSection);
-                                shape = Stb2ColSec.SecColumnS.Shape[idShape];
+                                idShape = colS.Id.IndexOf(idSection);
+                                shape = colS.Shape[idShape];
                                 break;
                             case FrameType.Girder:
                             case FrameType.Beam:
-                                idShape = Stb2ColSec.SecBeamS.Id.IndexOf(idSection);
-                                shape = Stb2ColSec.SecBeamS.Shape[idShape];
+                                idShape = beamS.Id.IndexOf(idSection);
+                                shape = beamS.Shape[idShape];
                                 break;
                             case FrameType.Brace:
-                                idShape = Stb2ColSec.SecBraceS.Id.IndexOf(idSection);
-                                shape = Stb2ColSec.SecBraceS.Shape[idShape];
+                                idShape = braceS.Id.IndexOf(idSection);
+                                shape = braceS.Shape[idShape];
                                 break;
                         }
-
-                        secIndex = Stb2ColSec.StbSecSteel.Name.IndexOf(shape);
-                        name = Stb2ColSec.StbSecSteel.Name[secIndex];
-                        p1 = Stb2ColSec.StbSecSteel.P1[secIndex];
-                        p2 = Stb2ColSec.StbSecSteel.P2[secIndex];
-                        p3 = Stb2ColSec.StbSecSteel.P3[secIndex];
-                        p4 = Stb2ColSec.StbSecSteel.P4[secIndex];
-                        shapeType = Stb2ColSec.StbSecSteel.ShapeType[secIndex];
+        
+                        secIndex = secSteel.Name.IndexOf(shape);
+                        name = secSteel.Name[secIndex];
+                        p1 = secSteel.P1[secIndex];
+                        p2 = secSteel.P2[secIndex];
+                        p3 = secSteel.P3[secIndex];
+                        p4 = secSteel.P4[secIndex];
+                        shapeType = secSteel.ShapeType[secIndex];
                         break;
                     }
                 }
@@ -111,7 +109,7 @@ namespace StbHopper.Util
                 sec.Append(new GH_String(p3.ToString()), ghPath);
                 sec.Append(new GH_String(p4.ToString()), ghPath);
             }
-
+        
             return sec;
         }
     }
