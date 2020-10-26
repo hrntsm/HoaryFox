4b5a7912d2debbb4033e7e57bb49b8d2b061621f
hiron <contact@hrntsm.com>
2020-10-26 11:26:06 +0900

    Clean FrameLines.cs's duplicate codes

--- HoaryFox/Member/FrameLines.cs
+++ HoaryFox/Member/FrameLines.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using STBReader;
 using STBReader.Model;
 using Rhino.Geometry;
+using STBReader.Member;
 
 namespace HoaryFox.Member
 {
@@ -19,85 +20,46 @@ namespace HoaryFox.Member
 
         public List<Line> Columns()
         {
-            var columns = new List<Line>();
-            for (var i = 0; i < _stbData.Columns.Id.Count; i++)
-            {
-                int idNodeStart = _nodes.Id.IndexOf(_stbData.Columns.IdNodeStart[i]);
-                int idNodeEnd = _stbData.Nodes.Id.IndexOf(_stbData.Columns.IdNodeEnd[i]);
-                var ptStart = new Point3d(_nodes.X[idNodeStart], _nodes.Y[idNodeStart], _nodes.Z[idNodeStart]);
-                var ptEnd = new Point3d(_nodes.X[idNodeEnd], _nodes.Y[idNodeEnd], _nodes.Z[idNodeEnd]);
-                columns.Add(new Line(ptStart, ptEnd));
-            }
-
-            return columns;
+            return CreateFrameLines(_stbData.Columns);
         }
 
         public List<Line> Girders()
         {
-            var girders = new List<Line>();
-            for (var i = 0; i < _stbData.Girders.Id.Count; i++)
-            {
-                int idNodeStart = _nodes.Id.IndexOf(_stbData.Girders.IdNodeStart[i]);
-                int idNodeEnd = _nodes.Id.IndexOf(_stbData.Girders.IdNodeEnd[i]);
-                var ptStart = new Point3d(_nodes.X[idNodeStart], _nodes.Y[idNodeStart], _nodes.Z[idNodeStart]);
-                var ptEnd = new Point3d(_nodes.X[idNodeEnd], _nodes.Y[idNodeEnd], _nodes.Z[idNodeEnd]);
-                girders.Add(new Line(ptStart, ptEnd));
-            }
-
-            return girders;
+            return CreateFrameLines(_stbData.Girders);
         }
 
         public List<Line> Posts()
         {
-            var posts = new List<Line>();
-            
-            for (var i = 0; i < _stbData.Posts.Id.Count; i++)
-            {
-                int idNodeStart = _nodes.Id.IndexOf(_stbData.Posts.IdNodeStart[i]);
-                int idNodeEnd = _nodes.Id.IndexOf(_stbData.Posts.IdNodeEnd[i]);
-                var ptStart = new Point3d(_nodes.X[idNodeStart], _nodes.Y[idNodeStart], _nodes.Z[idNodeStart]);
-                var ptEnd = new Point3d(_nodes.X[idNodeEnd], _nodes.Y[idNodeEnd], _nodes.Z[idNodeEnd]);
-                posts.Add(new Line(ptStart, ptEnd));
-            }
-
-            return posts;
+            return CreateFrameLines(_stbData.Posts);
         }
 
         public List<Line> Beams()
         {
-            var beams = new List<Line>();
-            
-            for (var i = 0; i < _stbData.Beams.Id.Count; i++)
-            {
-                int idNodeStart = _nodes.Id.IndexOf(_stbData.Beams.IdNodeStart[i]);
-                int idNodeEnd = _nodes.Id.IndexOf(_stbData.Beams.IdNodeEnd[i]);
-                var ptStart = new Point3d(_nodes.X[idNodeStart], _nodes.Y[idNodeStart], _nodes.Z[idNodeStart]);
-                var ptEnd = new Point3d(_nodes.X[idNodeEnd], _nodes.Y[idNodeEnd], _nodes.Z[idNodeEnd]);
-                beams.Add(new Line(ptStart, ptEnd));
-            }
-
-            return beams;
+            return CreateFrameLines(_stbData.Beams);
         }
 
         public List<Line> Braces()
         {
-            var braces = new List<Line>();
-
-            for (var i = 0; i < _stbData.Braces.Id.Count; i++)
-            {
-                int idNodeStart = _nodes.Id.IndexOf(_stbData.Braces.IdNodeStart[i]);
-                int idNodeEnd = _nodes.Id.IndexOf(_stbData.Braces.IdNodeEnd[i]);
-                var ptStart = new Point3d(_nodes.X[idNodeStart], _nodes.Y[idNodeStart], _nodes.Z[idNodeStart]);
-                var ptEnd = new Point3d(_nodes.X[idNodeEnd], _nodes.Y[idNodeEnd], _nodes.Z[idNodeEnd]);
-                braces.Add(new Line(ptStart, ptEnd));
-            }
-
-            return braces;
+            return CreateFrameLines(_stbData.Braces);
         }
 
         public List<Point3d> Nodes()
         {
             return _nodes.Pt.Select(point => new Point3d(point.X, point.Y, point.Z)).ToList();
+        }
+
+        private List<Line> CreateFrameLines(StbFrame frame)
+        {
+            var lines = new List<Line>();
+            for (var i = 0; i < frame.Id.Count; i++)
+            {
+                int idNodeStart = _nodes.Id.IndexOf(frame.IdNodeStart[i]);
+                int idNodeEnd = _stbData.Nodes.Id.IndexOf(frame.IdNodeEnd[i]);
+                var ptStart = new Point3d(_nodes.X[idNodeStart], _nodes.Y[idNodeStart], _nodes.Z[idNodeStart]);
+                var ptEnd = new Point3d(_nodes.X[idNodeEnd], _nodes.Y[idNodeEnd], _nodes.Z[idNodeEnd]);
+                lines.Add(new Line(ptStart, ptEnd));
+            }
+            return lines;
         }
     }
 }
