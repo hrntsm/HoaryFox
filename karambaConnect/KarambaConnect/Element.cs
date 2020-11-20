6d38f018145245fdf04a1ffe0993633d564c463d
hiron <contact@hrntsm.com>
2020-11-20 22:59:09 +0900

    Clean karamba element

--- karambaConnect/KarambaConnect/Element.cs
+++ karambaConnect/KarambaConnect/Element.cs
@@ -13,14 +13,11 @@ namespace HoaryFox.KarambaConnect
     {
         public static List<BuilderBeam> BuilderBeams(StbData stbData, List<string>[] k3Ids)
         {
-            var logger = new MessageLogger();
             var k3d = new KarambaCommon.Toolkit();
             var elems = new List<BuilderBeam>();
             StbNodes nodes = stbData.Nodes;
             
-            var k3Elems = new List<Line3>[2];
-            k3Elems[0] = new List<Line3>();
-            k3Elems[1] = new List<Line3>();
+            var k3Elems = new List<Line3>[2]{ new List<Line3>() , new List<Line3>() };
 
             foreach (StbFrame frame in new List<StbFrame>{ stbData.Columns, stbData.Girders, stbData.Braces} )
             {
@@ -31,19 +28,11 @@ namespace HoaryFox.KarambaConnect
                 {
                     int idNodeStart = nodes.Id.IndexOf(frame.IdNodeStart[i]);
                     int idNodeEnd = nodes.Id.IndexOf(frame.IdNodeEnd[i]);
-                    var p0 = new Karamba.Geometry.Point3(
-                        nodes.Position[idNodeStart].X / 1000d,
-                        nodes.Position[idNodeStart].Y / 1000d,
-                        nodes.Position[idNodeStart].Z / 1000d
-                    );
-                    var p1 = new Karamba.Geometry.Point3(
-                        nodes.Position[idNodeEnd].X / 1000d,
-                        nodes.Position[idNodeEnd].Y / 1000d,
-                        nodes.Position[idNodeEnd].Z / 1000d
-                    );
+                    var p0 = new Karamba.Geometry.Point3(nodes.Position[idNodeStart].X / 1000d, nodes.Position[idNodeStart].Y / 1000d, nodes.Position[idNodeStart].Z / 1000d);
+                    var p1 = new Karamba.Geometry.Point3(nodes.Position[idNodeEnd].X / 1000d, nodes.Position[idNodeEnd].Y / 1000d, nodes.Position[idNodeEnd].Z / 1000d);
                     k3Elems[elemIndex].Add(new Line3(p0, p1));
                 }
-                elems.AddRange(k3d.Part.LineToBeam(k3Elems[elemIndex], k3Ids[elemIndex], new List<CroSec>(), logger, out _, bending));
+                elems.AddRange(k3d.Part.LineToBeam(k3Elems[elemIndex], k3Ids[elemIndex], new List<CroSec>(), new MessageLogger(), out _, bending));
             }
 
             return elems;
