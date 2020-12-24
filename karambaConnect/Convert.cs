c6ea60f5b5202916c11e89265b6b64e36574a7e0
hiron <contact@hrntsm.com>
2020-12-24 17:00:36 +0900

    Fix unit

--- karambaConnect/Convert.cs
+++ karambaConnect/Convert.cs
@@ -6,12 +6,12 @@ namespace karambaConnect
     {
         public static STBDotNet.Geometry.Point3 ToStb(this Karamba.Geometry.Point3 kpt)
         {
-            return new STBDotNet.Geometry.Point3(kpt.X, kpt.Y, kpt.Z);
+            return new STBDotNet.Geometry.Point3(kpt.X, kpt.Y, kpt.Z) / 1000;
         }
 
         public static Karamba.Geometry.Point3 ToKaramba(this STBDotNet.Geometry.Point3 spt)
         {
-            return new Karamba.Geometry.Point3(spt.X, spt.Y, spt.Z);
+            return new Karamba.Geometry.Point3(spt.X, spt.Y, spt.Z) * 1000;
         }
 
         public static List<STBDotNet.Elements.StbModel.Node> ToStb(this List<Karamba.Nodes.Node> kNodes)
@@ -23,9 +23,9 @@ namespace karambaConnect
                 var sNode = new STBDotNet.Elements.StbModel.Node()
                 {
                     Id = kNode.ind,
-                    X = kNode.pos.X,
-                    Y = kNode.pos.Y,
-                    Z = kNode.pos.Z
+                    X = kNode.pos.X * 1000,
+                    Y = kNode.pos.Y * 1000,
+                    Z = kNode.pos.Z * 1000
                 };
                 sNodes.Add(sNode);
             }
