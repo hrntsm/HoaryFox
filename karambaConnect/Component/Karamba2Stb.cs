e21051bc49ad10560d86f261240033e1b6e46596
hiron <contact@hrntsm.com>
2020-12-27 22:41:12 +0900

    Add StbElement output

--- karambaConnect/Component/Karamba2Stb.cs
+++ karambaConnect/Component/Karamba2Stb.cs
@@ -2,6 +2,7 @@
 using Grasshopper.Kernel;
 using Karamba.Models;
 using Karamba.GHopper.Models;
+using karambaConnect.K2S;
 using STBDotNet.Elements;
 using STBDotNet.Serialization;
 
@@ -9,29 +10,24 @@ namespace karambaConnect.Component
 {
     public class Karamba2Stb:GH_Component
     {
-        private readonly string defaultOutPath =
-            Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\model.stb";
-
-
+        private readonly string _defaultOutPath =
+            Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\model.stb";
+        
         public Karamba2Stb()
           : base("Karamba to Stb", "K2S", "Convert Karamba model to ST-Bridge data and output.", "HoaryFox", "IO")
         {
         }
 
-        public override void ClearData()
-        {
-            base.ClearData();
-        }
-
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
             pManager.AddParameter(new Param_Model(), "Model", "Model", "Karamba model data", GH_ParamAccess.item);
-            pManager.AddTextParameter("Path", "Path", "Output path", GH_ParamAccess.item, defaultOutPath);
+            pManager.AddTextParameter("Path", "Path", "Output path", GH_ParamAccess.item, _defaultOutPath);
             pManager.AddBooleanParameter("Out?", "Out?", "If it is true, output stb file.", GH_ParamAccess.item, false);
         }
 
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
+            pManager.AddGenericParameter("Stb", "Stb", "StbModel Data", GH_ParamAccess.item);
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
@@ -50,26 +46,16 @@ namespace karambaConnect.Component
                 throw new ArgumentException("The input is not of type model!");
             }
             Model model = ghKModel.Value;
-            
+            StbElements elements = StbElement.GetData(model);
+
             if (isOutput)
             {
-                StbElements elements = GetStbData(model);
                 var sr = new Serializer();
                 sr.Serialize(elements, path);
             }
+            
+            DA.SetData(0, elements);
         }
-
-        private StbElements GetStbData(Model model)
-        {
-            var elem = new StbElements
-            {
-                Version = "1.4.00",
-                Common = K2S.StbCommon.Set(),
-                Model = K2S.StbModel.Set(model)
-            };
-            return elem;
-        }
-
 
         // protected override Bitmap Icon => karambaConnect.Properties.Resource.ToKaramba;
         public override Guid ComponentGuid => new Guid("38296D06-E47A-403F-BFE8-00E873A99CF8");
---
+++ karambaConnect/K2S/StbElement.cs
@@ -0,0 +1,19 @@
+﻿using Karamba.Models;
+using STBDotNet.Elements;
+
+namespace karambaConnect.K2S
+{
+    public static class StbElement
+    {
+        public static StbElements GetData(Model model)
+        {
+            var elem = new StbElements
+            {
+                Version = "1.4.00",
+                Common = StbCommon.Set(),
+                Model = StbModel.Set(model)
+            };
+            return elem;
+        }
+    }
+}
