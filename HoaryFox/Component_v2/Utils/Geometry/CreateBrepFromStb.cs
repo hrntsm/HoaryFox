18d1ec7c01a7de0b3dcf420db3e462e5a1515389
hiron <contact@hrntsm.com>
2021-07-12 00:02:39 +0900

    Add H column brep output

--- HoaryFox/Component_v2/Utils/Geometry/CreateBrepFromStb.cs
+++ HoaryFox/Component_v2/Utils/Geometry/CreateBrepFromStb.cs
@@ -48,34 +48,13 @@ namespace HoaryFox.Component_v2.Utils.Geometry
                 {
                     case StbColumnKind_structure.RC:
                         StbSecColumn_RC rcSec = _sections.StbSecColumn_RC.First(sec => sec.id == column.id_section);
-
-                        switch (rcSec.StbSecFigureColumn_RC.Item)
-                        {
-                            case StbSecColumn_RC_Rect figure:
-                                curveList.Add(new PolylineCurve(SectionCornerPoints.Column(sectionPoints[0],
-                                    figure.width_X, figure.width_Y)));
-                                curveList.Add(new PolylineCurve(SectionCornerPoints.Column(sectionPoints[3],
-                                    figure.width_X, figure.width_Y)));
-                                break;
-                            case StbSecColumn_RC_Circle figure:
-                                curveList.Add(new ArcCurve(new Circle(sectionPoints[0], figure.D / 2d)));
-                                curveList.Add(new ArcCurve(new Circle(sectionPoints[3], figure.D / 2d)));
-                                break;
-                            default:
-                                throw new Exception();
-                        }
-
+                        object figure = rcSec.StbSecFigureColumn_RC.Item;
+                        curveList = SecRcColumnToCurves(figure, sectionPoints);
                         break;
                     case StbColumnKind_structure.S:
                         StbSecColumn_S sSec = _sections.StbSecColumn_S.First(sec => sec.id == column.id_section);
                         object[] figures = sSec.StbSecSteelFigureColumn_S.Items;
-
-                        switch (figures[0])
-                        {
-                            case StbSecSteelColumn_S_Same _:
-                                curveList = SteelSecToCurves(figures, sectionPoints);
-                                break;
-                        }
+                        curveList = SecSteelColumnToCurves(figures, sectionPoints);
                         break;
                     case StbColumnKind_structure.SRC:
                     case StbColumnKind_structure.CFT:
@@ -88,7 +67,7 @@ namespace HoaryFox.Component_v2.Utils.Geometry
                 brep = Brep.CreateFromLoft(curveList, Point3d.Unset, Point3d.Unset, LoftType.Straight, false)[0]
                     .CapPlanarHoles(_tolerance[0]);
 
-                Vector3d memberAxis = sectionPoints[1] - sectionPoints[0];
+                Vector3d memberAxis = sectionPoints[3] - sectionPoints[0];
                 Vector3d rotateAxis = Vector3d.CrossProduct(Vector3d.ZAxis, memberAxis);
                 double angle = Vector3d.VectorAngle(Vector3d.ZAxis, memberAxis);
                 brep.Rotate(column.rotate, Vector3d.ZAxis, sectionPoints[0]); // 要素軸での回転
@@ -99,26 +78,92 @@ namespace HoaryFox.Component_v2.Utils.Geometry
             return brepList;
         }
 
-        private List<Curve> SteelSecToCurves(object[] figures, IReadOnlyList<Point3d> sectionPoints)
+        private List<Curve> SecRcColumnToCurves(object figure, IReadOnlyList<Point3d> sectionPoints)
+        {
+            var curveList = new List<Curve>();
+
+            switch (figure)
+            {
+                case StbSecColumn_RC_Rect rect:
+                    curveList.Add(new PolylineCurve(
+                        SectionCornerPoints.ColumnRect(sectionPoints[0], rect.width_X, rect.width_Y)));
+                    curveList.Add(new PolylineCurve(
+                        SectionCornerPoints.ColumnRect(sectionPoints[3], rect.width_X, rect.width_Y)));
+                    break;
+                case StbSecColumn_RC_Circle circle:
+                    curveList.Add(new ArcCurve(new Circle(sectionPoints[0], circle.D / 2d)));
+                    curveList.Add(new ArcCurve(new Circle(sectionPoints[3], circle.D / 2d)));
+                    break;
+                default:
+                    throw new Exception();
+            }
+
+            return curveList;
+        }
+
+        private List<Curve> SecSteelColumnToCurves(IReadOnlyList<object> figures, IReadOnlyList<Point3d> sectionPoints)
+        {
+            var curveList = new List<Curve>();
+
+            switch (figures.Count)
+            {
+                case 1:
+                    var same = figures[0] as StbSecSteelColumn_S_Same;
+                    curveList.Add(GetSteelSec(same.shape, sectionPoints[0], SectionType.Column));
+                    curveList.Add(GetSteelSec(same.shape, sectionPoints[3], SectionType.Column));
+                    break;
+                case 2:
+                    var notSames = new[] { figures[0] as StbSecSteelColumn_S_NotSame, figures[1] as StbSecSteelColumn_S_NotSame };
+                    StbSecSteelColumn_S_NotSame nsBottom = notSames.First(item => item.pos == StbSecSteelColumn_S_NotSamePos.BOTTOM);
+                    StbSecSteelColumn_S_NotSame nsTop = notSames.First(item => item.pos == StbSecSteelColumn_S_NotSamePos.TOP);
+                    curveList.Add(GetSteelSec(nsBottom.shape, sectionPoints[0], SectionType.Column));
+                    if (sectionPoints[1].Z > sectionPoints[0].Z)
+                    {
+                        curveList.Add(GetSteelSec(nsBottom.shape, sectionPoints[1], SectionType.Column));
+                        curveList.Add(GetSteelSec(nsTop.shape, sectionPoints[1], SectionType.Column));
+                    }
+                    else
+                    {
+                        curveList.Add(GetSteelSec(nsBottom.shape, sectionPoints[2], SectionType.Column));
+                        curveList.Add(GetSteelSec(nsTop.shape, sectionPoints[2], SectionType.Column));
+                    }
+                    curveList.Add(GetSteelSec(nsTop.shape, sectionPoints[3], SectionType.Column));
+                    break;
+                case 3:
+                    var three = new[] { figures[0] as StbSecSteelColumn_S_ThreeTypes, figures[1] as StbSecSteelColumn_S_ThreeTypes, figures[2] as StbSecSteelColumn_S_ThreeTypes };
+                    StbSecSteelColumn_S_ThreeTypes tBottom = three.First(item => item.pos == StbSecSteelColumn_S_ThreeTypesPos.BOTTOM);
+                    StbSecSteelColumn_S_ThreeTypes tCenter = three.First(item => item.pos == StbSecSteelColumn_S_ThreeTypesPos.CENTER);
+                    StbSecSteelColumn_S_ThreeTypes tTop = three.First(item => item.pos == StbSecSteelColumn_S_ThreeTypesPos.TOP);
+                    curveList.Add(GetSteelSec(tBottom.shape, sectionPoints[0], SectionType.Column));
+                    curveList.Add(GetSteelSec(tCenter.shape, sectionPoints[1], SectionType.Column));
+                    curveList.Add(GetSteelSec(tCenter.shape, sectionPoints[2], SectionType.Column));
+                    curveList.Add(GetSteelSec(tTop.shape, sectionPoints[3], SectionType.Column));
+                    break;
+                default:
+                    throw new ArgumentException("Unmatched StbSecSteelColumn_S");
+            }
+
+            return curveList;
+        }
+
+        private List<Curve> SecSteelBeamToCurves(object[] figures, IReadOnlyList<Point3d> sectionPoints)
         {
             var curveList = new List<Curve>();
 
             switch (figures)
             {
-                case StbSecSteelColumn_S_Same[] same: // 1断面
-                    curveList.Add(GetSteelSec(same[0].shape, sectionPoints[0], SectionType.Column));
-                    curveList.Add(GetSteelSec(same[0].shape, sectionPoints[3], SectionType.Column));
+                case StbSecSteelBeam_S_Straight[] straights:
                     break;
-                case StbSecSteelColumn_S_NotSame[] notSames: // 2断面
-                    curveList.Add(GetSteelSec(notSames[0].shape, sectionPoints[0], SectionType.Column));
-                    curveList.Add(GetSteelSec(notSames[0].shape, sectionPoints[3], SectionType.Column));
+                case StbSecSteelBeam_S_Taper[] tapers:
                     break;
-                case StbSecSteelColumn_S_ThreeTypes[] three: // ３断面
-                    curveList.Add(GetSteelSec(three[0].shape, sectionPoints[0], SectionType.Column));
-                    curveList.Add(GetSteelSec(three[0].shape, sectionPoints[3], SectionType.Column));
+                case StbSecSteelBeam_S_Joint[] joints:
+                    break;
+                case StbSecSteelBeam_S_Haunch[] haunches:
+                    break;
+                case StbSecSteelBeam_S_FiveTypes[] five:
                     break;
                 default:
-                    throw new ArgumentException("Unmatched StbSecSteelColumn_S");
+                    throw new ArgumentException("Unmatched StbSecSteelBeam_S");
             }
 
             return curveList;
@@ -137,7 +182,7 @@ namespace HoaryFox.Component_v2.Utils.Geometry
                         switch (type)
                         {
                             case SectionType.Column:
-                                return new PolylineCurve(SectionCornerPoints.Column(point, box.A, box.B));
+                                return new PolylineCurve(SectionCornerPoints.ColumnRect(point, box.B, box.A));
                             case SectionType.Beam:
                                 break;
                             case SectionType.Brace:
@@ -158,7 +203,7 @@ namespace HoaryFox.Component_v2.Utils.Geometry
                         switch (type)
                         {
                             case SectionType.Column:
-                                return new PolylineCurve(SectionCornerPoints.Column(point, box.A, box.B));
+                                return new PolylineCurve(SectionCornerPoints.ColumnRect(point, box.B, box.A));
                             case SectionType.Beam:
                                 break;
                             case SectionType.Brace:
@@ -170,13 +215,50 @@ namespace HoaryFox.Component_v2.Utils.Geometry
                 }
             }
 
-            // foreach (StbSecBuildH buildH in _sections.StbSecSteel.StbSecBuildH)
-            // {
-            //     if (buildH.name == shape)
-            //     {
-            //         
-            //     }
-            // }
+            if (secSteel.StbSecBuildH != null)
+            {
+                foreach (StbSecBuildH buildH in _sections.StbSecSteel.StbSecBuildH)
+                {
+                    if (buildH.name == shape)
+                    {
+                        switch (type)
+                        {
+                            case SectionType.Column:
+                                return new PolylineCurve(SectionCornerPoints.ColumnH(point, buildH.B, buildH.A,
+                                    buildH.t1, buildH.t2));
+                            case SectionType.Beam:
+                                break;
+                            case SectionType.Brace:
+                                break;
+                            default:
+                                throw new ArgumentOutOfRangeException(nameof(type), type, null);
+                        }
+                    }
+                }
+            }
+
+            if (secSteel.StbSecRollH != null)
+            {
+                foreach (StbSecRollH rollH in _sections.StbSecSteel.StbSecRollH)
+                {
+                    if (rollH.name == shape)
+                    {
+                        switch (type)
+                        {
+                            case SectionType.Column:
+                                return new PolylineCurve(SectionCornerPoints.ColumnH(point, rollH.B, rollH.A, rollH.t1, rollH.t2));
+                            case SectionType.Beam:
+                                break;
+                            case SectionType.Brace:
+                                break;
+                            default:
+                                throw new ArgumentOutOfRangeException(nameof(type), type, null);
+                        }
+                    }
+                }
+            }
+
+            // TODO: Box と H 以外の断面を実装する
 
             throw new ArgumentException("There are no matching steel section");
         }
--- HoaryFox/Component_v2/Utils/Geometry/SectionCornerPoints.cs
+++ HoaryFox/Component_v2/Utils/Geometry/SectionCornerPoints.cs
@@ -6,44 +6,71 @@ namespace HoaryFox.Component_v2.Utils.Geometry
 {
     public static class SectionCornerPoints
     {
-        //  Z        3 - 2
+        //  Y        3 - 2
         //  ^        |   |
-        //  o > Y    0 - 1
-        public static Point3d[] Column(Point3d node, double width, double height)
+        //  o > X    0 - 1
+        public static Point3d[] ColumnRect(Point3d pt, double width, double height)
         {
             var points = new[]
             {
-                new Point3d(node.X - width / 2, node.Y - height / 2, node.Z),
-                new Point3d(node.X + width / 2, node.Y - height / 2, node.Z),
-                new Point3d(node.X + width / 2, node.Y + height / 2, node.Z),
-                new Point3d(node.X - width / 2, node.Y + height / 2, node.Z),
-                new Point3d(node.X - width / 2, node.Y - height / 2, node.Z),
+                new Point3d(pt.X - width / 2, pt.Y - height / 2, pt.Z),
+                new Point3d(pt.X + width / 2, pt.Y - height / 2, pt.Z),
+                new Point3d(pt.X + width / 2, pt.Y + height / 2, pt.Z),
+                new Point3d(pt.X - width / 2, pt.Y + height / 2, pt.Z),
+                new Point3d(pt.X - width / 2, pt.Y - height / 2, pt.Z),
             };
 
             return points;
         }
 
-        public static List<Point3d> Girder(Point3d node, double width, double height, double angle)
+        public static List<Point3d> Girder(Point3d pt, double width, double height, double angle)
         {
             var points = new List<Point3d>
             {
-                new Point3d(node.X + width / 2 * Math.Sin(angle), node.Y + width / 2 * Math.Cos(angle), node.Z - height),
-                new Point3d(node.X - width / 2 * Math.Sin(angle), node.Y - width / 2 * Math.Cos(angle), node.Z - height),
-                new Point3d(node.X + width / 2 * Math.Sin(angle), node.Y + width / 2 * Math.Cos(angle), node.Z),
-                new Point3d(node.X - width / 2 * Math.Sin(angle), node.Y - width / 2 * Math.Cos(angle), node.Z)
+                new Point3d(pt.X + width / 2 * Math.Sin(angle), pt.Y + width / 2 * Math.Cos(angle), pt.Z - height),
+                new Point3d(pt.X - width / 2 * Math.Sin(angle), pt.Y - width / 2 * Math.Cos(angle), pt.Z - height),
+                new Point3d(pt.X + width / 2 * Math.Sin(angle), pt.Y + width / 2 * Math.Cos(angle), pt.Z),
+                new Point3d(pt.X - width / 2 * Math.Sin(angle), pt.Y - width / 2 * Math.Cos(angle), pt.Z)
             };
 
             return points;
         }
 
-        public static List<Point3d> Brace(Point3d node, double width, double angle)
+        public static List<Point3d> Brace(Point3d pt, double width, double angle)
         {
             var points = new List<Point3d>
             {
-                new Point3d(node.X + width / 2 * Math.Sin(angle), node.Y + width / 2 * Math.Cos(angle), node.Z - width / 2),
-                new Point3d(node.X - width / 2 * Math.Sin(angle), node.Y - width / 2 * Math.Cos(angle), node.Z - width / 2),
-                new Point3d(node.X + width / 2 * Math.Sin(angle), node.Y + width / 2 * Math.Cos(angle), node.Z + width / 2),
-                new Point3d(node.X - width / 2 * Math.Sin(angle), node.Y - width / 2 * Math.Cos(angle), node.Z + width / 2)
+                new Point3d(pt.X + width / 2 * Math.Sin(angle), pt.Y + width / 2 * Math.Cos(angle), pt.Z - width / 2),
+                new Point3d(pt.X - width / 2 * Math.Sin(angle), pt.Y - width / 2 * Math.Cos(angle), pt.Z - width / 2),
+                new Point3d(pt.X + width / 2 * Math.Sin(angle), pt.Y + width / 2 * Math.Cos(angle), pt.Z + width / 2),
+                new Point3d(pt.X - width / 2 * Math.Sin(angle), pt.Y - width / 2 * Math.Cos(angle), pt.Z + width / 2)
+            };
+
+            return points;
+        }
+
+        //           7 - - - - 6
+        //           8 - 9 4 - 5
+        //  Y            | |    
+        //  ^        11-10 3 - 2
+        //  o > X    0 - - - - 1
+        public static Point3d[] ColumnH(Point3d pt, double height, double width, double tw, double tf)
+        {
+            var points = new[]
+            {
+                new Point3d(pt.X - width / 2, pt.Y - height / 2, pt.Z),
+                new Point3d(pt.X + width / 2, pt.Y - height / 2, pt.Z),
+                new Point3d(pt.X + width / 2, pt.Y - height / 2 + tf, pt.Z),
+                new Point3d(pt.X + tw / 2, pt.Y - height / 2 + tf, pt.Z),
+                new Point3d(pt.X + tw / 2, pt.Y + height / 2 - tf, pt.Z),
+                new Point3d(pt.X + width / 2, pt.Y + height / 2 - tf, pt.Z),
+                new Point3d(pt.X + width / 2, pt.Y + height / 2, pt.Z),
+                new Point3d(pt.X - width / 2, pt.Y + height / 2, pt.Z),
+                new Point3d(pt.X - width / 2, pt.Y + height / 2 - tf, pt.Z),
+                new Point3d(pt.X - tw / 2, pt.Y + height / 2 - tf, pt.Z),
+                new Point3d(pt.X - tw / 2, pt.Y - height / 2 + tf, pt.Z),
+                new Point3d(pt.X - width / 2, pt.Y - height / 2 + tf, pt.Z),
+                new Point3d(pt.X - width / 2, pt.Y - height / 2, pt.Z)
             };
 
             return points;
