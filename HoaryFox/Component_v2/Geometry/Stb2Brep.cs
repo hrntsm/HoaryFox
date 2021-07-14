44f04920f86f144f0b8c24572dcb42363a954be0
hiron <contact@hrntsm.com>
2021-07-14 21:45:47 +0900

    Clean Make Brep

--- HoaryFox/Component_v2/Geometry/Stb2Brep.cs
+++ HoaryFox/Component_v2/Geometry/Stb2Brep.cs
@@ -62,13 +62,14 @@ namespace HoaryFox.Component_v2.Geometry
 
         private void MakeBrep()
         {
+            StbMembers member = _stBridge.StbModel.StbMembers;
             var brepFromStb = new CreateBrepFromStb(_stBridge.StbModel.StbSections, _stBridge.StbModel.StbNodes, new[] { DocumentTolerance(), DocumentAngleTolerance() });
-            _brepList.Add(brepFromStb.Column(_stBridge.StbModel.StbMembers.StbColumns));
-            _brepList.Add(brepFromStb.Girder(_stBridge.StbModel.StbMembers.StbGirders));
-            _brepList.Add(brepFromStb.Post(_stBridge.StbModel.StbMembers.StbPosts));
-            _brepList.Add(brepFromStb.Beam(_stBridge.StbModel.StbMembers.StbBeams));
+            _brepList.Add(brepFromStb.Column(member.StbColumns));
+            _brepList.Add(brepFromStb.Girder(member.StbGirders));
+            _brepList.Add(brepFromStb.Post(member.StbPosts));
+            _brepList.Add(brepFromStb.Beam(member.StbBeams));
 
-            for (var i = 0; i < 5; i++)
+            for (var i = 0; i < 3; i++)
             {
                 _brepList.Add(new List<Brep>());
             }
