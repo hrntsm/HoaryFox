4e414c933992460e83466357054a96161f30ebe2
hiron <contact@hrntsm.com>
2021-08-08 11:38:48 +0900

    Clean code

--- KarambaConnect/S2K/ElementBuilder.cs
+++ KarambaConnect/S2K/ElementBuilder.cs
@@ -10,57 +10,65 @@ namespace KarambaConnect.S2K
 {
     public static class ElementBuilder
     {
-        public static List<BuilderBeam> BuilderBeams(ST_BRIDGE stBridge, List<string>[] k3Ids)
+        public static List<BuilderBeam> BuilderBeams(StbModel model, List<string>[] k3dIds)
         {
-            var k3d = new KarambaCommon.Toolkit();
-            var elems = new List<BuilderBeam>();
-            var k3Elems = new[] { new List<Line3>(), new List<Line3>() };
-            StbNode[] nodes = stBridge.StbModel.StbNodes;
-            StbMembers members = stBridge.StbModel.StbMembers;
+            var k3dBeams = new List<BuilderBeam>();
+            StbNode[] nodes = model.StbNodes;
+            StbMembers members = model.StbMembers;
 
-            StbColumnToKarambaLine3(members.StbColumns, nodes, k3Elems);
-            StbGirderToKarambaLine3(members.StbGirders, nodes, k3Elems);
-            StbBraceToKarambaLine3(members.StbBraces, nodes, k3Elems);
-            elems.AddRange(k3d.Part.LineToBeam(k3Elems[0], k3Ids[0], new List<CroSec>(), new MessageLogger(), out _, true));
-            elems.AddRange(k3d.Part.LineToBeam(k3Elems[1], k3Ids[1], new List<CroSec>(), new MessageLogger(), out _, true));
+            k3dBeams.AddRange(StbColumnToK3dBeam(k3dIds[0], nodes, members.StbColumns));
+            k3dBeams.AddRange(StbGirderToK3dBeam(k3dIds[1], nodes, members.StbGirders));
+            k3dBeams.AddRange(StbBraceToK3dBeam(k3dIds[2], nodes, members.StbBraces));
 
-            return elems;
+            return k3dBeams;
         }
 
-        private static void StbColumnToKarambaLine3(StbColumn[] columns, StbNode[] nodes, List<Line3>[] k3Elems)
+        private static List<BuilderBeam> StbColumnToK3dBeam(List<string> k3Ids, StbNode[] nodes, IEnumerable<StbColumn> columns)
         {
+            var k3dLine3s = new List<Line3>();
+            var k3dKit = new KarambaCommon.Toolkit();
             foreach (StbColumn column in columns)
             {
                 StbNode nodeStart = nodes.First(node => node.id == column.id_node_bottom);
                 StbNode nodeEnd = nodes.First(node => node.id == column.id_node_top);
                 var ptFrom = new Point3(nodeStart.X / 1000d, nodeStart.Y / 1000d, nodeStart.Z / 1000d);
                 var ptTo = new Point3(nodeEnd.X / 1000d, nodeEnd.Y / 1000d, nodeEnd.Z / 1000d);
-                k3Elems[0].Add(new Line3(ptFrom, ptTo));
+                k3dLine3s.Add(new Line3(ptFrom, ptTo));
             }
+
+            return k3dKit.Part.LineToBeam(k3dLine3s, k3Ids, new List<CroSec>(), new MessageLogger(), out _, true);
         }
 
-        private static void StbGirderToKarambaLine3(StbGirder[] girders, StbNode[] nodes, List<Line3>[] k3Elems)
+        private static List<BuilderBeam> StbGirderToK3dBeam(List<string> k3Ids, StbNode[] nodes, IEnumerable<StbGirder> girders)
         {
+            var k3dLine3s = new List<Line3>();
+            var k3dKit = new KarambaCommon.Toolkit();
             foreach (StbGirder girder in girders)
             {
                 StbNode nodeStart = nodes.First(node => node.id == girder.id_node_start);
                 StbNode nodeEnd = nodes.First(node => node.id == girder.id_node_end);
                 var ptFrom = new Point3(nodeStart.X / 1000d, nodeStart.Y / 1000d, nodeStart.Z / 1000d);
                 var ptTo = new Point3(nodeEnd.X / 1000d, nodeEnd.Y / 1000d, nodeEnd.Z / 1000d);
-                k3Elems[0].Add(new Line3(ptFrom, ptTo));
+                k3dLine3s.Add(new Line3(ptFrom, ptTo));
             }
+
+            return k3dKit.Part.LineToBeam(k3dLine3s, k3Ids, new List<CroSec>(), new MessageLogger(), out _, true);
         }
 
-        private static void StbBraceToKarambaLine3(StbBrace[] braces, StbNode[] nodes, List<Line3>[] k3Elems)
+        private static List<BuilderBeam> StbBraceToK3dBeam(List<string> k3Ids, StbNode[] nodes, IEnumerable<StbBrace> braces)
         {
+            var k3dLine3s = new List<Line3>();
+            var k3dKit = new KarambaCommon.Toolkit();
             foreach (StbBrace brace in braces)
             {
                 StbNode nodeStart = nodes.First(node => node.id == brace.id_node_start);
                 StbNode nodeEnd = nodes.First(node => node.id == brace.id_node_end);
                 var ptFrom = new Point3(nodeStart.X / 1000d, nodeStart.Y / 1000d, nodeStart.Z / 1000d);
                 var ptTo = new Point3(nodeEnd.X / 1000d, nodeEnd.Y / 1000d, nodeEnd.Z / 1000d);
-                k3Elems[1].Add(new Line3(ptFrom, ptTo));
+                k3dLine3s.Add(new Line3(ptFrom, ptTo));
             }
+
+            return k3dKit.Part.LineToBeam(k3dLine3s, k3Ids, new List<CroSec>(), new MessageLogger(), out _, true);
         }
     }
 }
