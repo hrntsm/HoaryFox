74039d868765dc5b20185caa077041036a56183d
hiron <contact@hrntsm.com>
2021-11-03 21:45:53 +0900

    Add Sort by materials feature

---
+++ HoaryFox/Component/Check/MaterialType.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+using HoaryFox.Properties;
+using STBDotNet.v202;
+
+namespace HoaryFox.Component.Check
+{
+    public class MaterialType : GH_Component
+    {
+        private ST_BRIDGE _stBridge;
+        private readonly GH_Structure<GH_String>[] _materialTypeList = new GH_Structure<GH_String>[7];
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        public MaterialType()
+          : base("Material Type", "MatType",
+              "Check material type",
+              "HoaryFox", "Check")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Data", "D", "input ST-Bridge Data", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Columns", "Col", "output StbColumns to Brep", GH_ParamAccess.tree);
+            pManager.AddTextParameter("Girders", "Gird", "output StbGirders to Brep", GH_ParamAccess.tree);
+            pManager.AddTextParameter("Posts", "Pst", "output StbPosts to Brep", GH_ParamAccess.tree);
+            pManager.AddTextParameter("Beams", "Bm", "output StbBeams to Brep", GH_ParamAccess.tree);
+            pManager.AddTextParameter("Braces", "Brc", "output StbBraces to Brep", GH_ParamAccess.tree);
+            pManager.AddTextParameter("Slabs", "Slb", "output StbSlabs to Brep", GH_ParamAccess.tree);
+            pManager.AddTextParameter("Walls", "Wl", "output StbWalls to Brep", GH_ParamAccess.tree);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            if (!dataAccess.GetData("Data", ref _stBridge)) { return; }
+
+            Dictionary<string, string>[][] infoArray = Utils.TagUtils.GetAllSectionInfoArray(_stBridge.StbModel.StbMembers, _stBridge.StbModel.StbSections);
+
+            foreach ((Dictionary<string, string>[] infoDict, int index) in infoArray.Select((dict, index) => (dict, index)))
+            {
+                var materialTypes = new GH_Structure<GH_String>();
+                foreach ((Dictionary<string, string> info, int itemIndex) in infoDict.Select((dict, itemIndex) => (dict, itemIndex)))
+                {
+                    materialTypes.Append(new GH_String(info["kind_structure"]), new GH_Path(0, itemIndex));
+                }
+                _materialTypeList[index] = materialTypes;
+            }
+
+            for (var i = 0; i < 7; i++)
+            {
+                dataAccess.SetDataTree(i, _materialTypeList[i]);
+            }
+        }
+
+        protected override Bitmap Icon => null;
+        public override Guid ComponentGuid => new Guid("b6fde031-3532-40bb-a276-f61c36cdee90");
+    }
+}
---
+++ HoaryFox/Component/Check/SortByMaterials.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+
+namespace HoaryFox.Component.Check
+{
+    public class SortByMaterials : GH_Component
+    {
+        public override GH_Exposure Exposure => GH_Exposure.secondary;
+
+        public SortByMaterials()
+          : base("Sort by materials", "SortByMat",
+              "Sort geometry by material",
+              "HoaryFox", "Check")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddBrepParameter("Geometry", "Geo", "Geometry", GH_ParamAccess.tree);
+            pManager.AddTextParameter("Materials", "Mats", "Geometry material info", GH_ParamAccess.tree);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddBrepParameter("RC", "RC", "output RC geometry", GH_ParamAccess.tree);
+            pManager.AddBrepParameter("S", "S", "output S geometry", GH_ParamAccess.tree);
+            pManager.AddBrepParameter("SRC", "SRC", "output SRC geometry", GH_ParamAccess.tree);
+            pManager.AddBrepParameter("CFT", "CFT", "output CFT geometry", GH_ParamAccess.tree);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            if (!dataAccess.GetDataTree(0, out GH_Structure<GH_Brep> breps)) { return; }
+            if (!dataAccess.GetDataTree(1, out GH_Structure<GH_String> mats)) { return; }
+
+            if (breps.Paths.Count != mats.Paths.Count)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Geometry and materials must have the same number of items");
+                return;
+            }
+
+            var results = new GH_Structure<GH_Brep>[4];
+            for (var i = 0; i < 4; i++)
+            {
+                results[i] = new GH_Structure<GH_Brep>();
+            }
+
+            for (var i = 0; i < breps.PathCount; i++)
+            {
+                switch (mats.Branches[i][0].ToString())
+                {
+                    case "RC":
+                        results[0].Append(breps.Branches[i][0].DuplicateBrep(), breps.Paths[i]);
+                        break;
+                    case "S":
+                        results[1].Append(breps.Branches[i][0].DuplicateBrep(), breps.Paths[i]);
+                        break;
+                    case "SRC":
+                        results[2].Append(breps.Branches[i][0].DuplicateBrep(), breps.Paths[i]);
+                        break;
+                    case "CFT":
+                        results[3].Append(breps.Branches[i][0].DuplicateBrep(), breps.Paths[i]);
+                        break;
+                    default:
+                        AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Unknown material type");
+                        return;
+                }
+            }
+
+            for (var i = 0; i < 4; i++)
+            {
+                dataAccess.SetDataTree(i, results[i]);
+            }
+        }
+
+        protected override Bitmap Icon => null;
+        public override Guid ComponentGuid => new Guid("a4bcebdb-2c9c-41e4-8328-e270ea70ed7b");
+    }
+}
