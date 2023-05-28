e6f0c3903aa3b732001df69b4b09fd549a2fe3d2
hrntsm <contact@hrntsm.com>
2023-05-28 13:36:50 +0900

    Fix same d extended pile bake error

--- HoaryFox/RH7/Component/Utils/Geometry/BrepMaker/Pile.cs
+++ HoaryFox/RH7/Component/Utils/Geometry/BrepMaker/Pile.cs
@@ -61,44 +61,92 @@ namespace HoaryFox.Component.Utils.Geometry.BrepMaker
         {
             var curveList = new List<SectionCurve>();
             Vector3d[] localAxis = Utils.CreateLocalAxis(sectionPoints);
-
             switch (figure)
             {
                 case StbSecPile_RC_Straight straight:
-                    curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[0], straight.D, localAxis[0]));
-                    curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[3], straight.D, localAxis[0]));
+                    PileStraight(sectionPoints, curveList, localAxis, straight.D);
                     break;
                 case StbSecPile_RC_ExtendedFoot foot:
-                    var footPos1 = sectionPoints[3] - axis * foot.length_extended_foot;
-                    var footPos2 = footPos1 - axis * (foot.D_extended_foot - foot.D_axial) / 2d / Math.Tan(Math.PI / 180 * foot.angle_extended_foot_taper);
-                    curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[0], foot.D_axial, localAxis[0]));
-                    curveList.Add(SectionCurve.CreateSolidColumnCircle(footPos2, foot.D_axial, localAxis[0]));
-                    curveList.Add(SectionCurve.CreateSolidColumnCircle(footPos1, foot.D_extended_foot, localAxis[0]));
-                    curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[3], foot.D_extended_foot, localAxis[0]));
+                    if (foot.D_axial == foot.D_extended_foot)
+                    {
+                        PileStraight(sectionPoints, curveList, localAxis, foot.D_axial);
+                    }
+                    else
+                    {
+                        PileExtendedFoot(sectionPoints, axis, curveList, localAxis, foot.D_axial, foot.D_extended_foot, foot.length_extended_foot, foot.angle_extended_foot_taper);
+                    }
                     break;
                 case StbSecPile_RC_ExtendedTop top:
-                    var topPos1 = sectionPoints[1] + axis * (top.D_extended_top - top.D_axial) / 2d / Math.Tan(Math.PI / 180 * top.angle_extended_top_taper);
-                    curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[0], top.D_extended_top, localAxis[0]));
-                    curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[1], top.D_extended_top, localAxis[0]));
-                    curveList.Add(SectionCurve.CreateSolidColumnCircle(topPos1, top.D_axial, localAxis[0]));
-                    curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[3], top.D_axial, localAxis[0]));
+                    if (top.D_axial == top.D_extended_top)
+                    {
+                        PileStraight(sectionPoints, curveList, localAxis, top.D_axial);
+                    }
+                    else
+                    {
+                        PileExtendedTop(sectionPoints, axis, curveList, localAxis, top.D_extended_top, top.D_axial, top.angle_extended_top_taper);
+                    }
                     break;
                 case StbSecPile_RC_ExtendedTopFoot both:
-                    var bothPos1 = sectionPoints[1] + axis * (both.D_extended_top - both.D_axial) / 2d / Math.Tan(Math.PI / 180 * both.angle_extended_top_taper);
-                    var bothPos2 = sectionPoints[3] - axis * both.length_extended_foot;
-                    var bothPos3 = bothPos2 - axis * (both.D_extended_foot - both.D_axial) / 2d / Math.Tan(Math.PI / 180 * both.angle_extended_foot_taper);
-                    curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[0], both.D_extended_top, localAxis[0]));
-                    curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[1], both.D_extended_top, localAxis[0]));
-                    curveList.Add(SectionCurve.CreateSolidColumnCircle(bothPos1, both.D_axial, localAxis[0]));
-                    curveList.Add(SectionCurve.CreateSolidColumnCircle(bothPos3, both.D_axial, localAxis[0]));
-                    curveList.Add(SectionCurve.CreateSolidColumnCircle(bothPos2, both.D_extended_foot, localAxis[0]));
-                    curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[3], both.D_extended_foot, localAxis[0]));
+                    if (both.D_axial == both.D_extended_foot && both.D_axial == both.D_extended_top)
+                    {
+                        PileStraight(sectionPoints, curveList, localAxis, both.D_axial);
+                    }
+                    else if (both.D_axial == both.D_extended_foot)
+                    {
+                        PileExtendedTop(sectionPoints, axis, curveList, localAxis, both.D_extended_top, both.D_axial, both.angle_extended_top_taper);
+                    }
+                    else if (both.D_axial == both.D_extended_top)
+                    {
+                        PileExtendedFoot(sectionPoints, axis, curveList, localAxis, both.D_axial, both.D_extended_foot, both.length_extended_foot, both.angle_extended_foot_taper);
+                    }
+                    else
+                    {
+                        PileExtendedTopFoot(sectionPoints, axis, curveList, localAxis, both);
+                    }
                     break;
                 default:
                     throw new ArgumentException();
             }
 
             return curveList.ToArray();
+        }
+
+        private static void PileExtendedTopFoot(IReadOnlyList<Point3d> sectionPoints, Vector3d axis, List<SectionCurve> curveList, Vector3d[] localAxis, StbSecPile_RC_ExtendedTopFoot both)
+        {
+            var bothPos1 = sectionPoints[1] + axis * (both.D_extended_top - both.D_axial) / 2d / Math.Tan(Math.PI / 180 * both.angle_extended_top_taper);
+            var bothPos2 = sectionPoints[3] - axis * both.length_extended_foot;
+            var bothPos3 = bothPos2 - axis * (both.D_extended_foot - both.D_axial) / 2d / Math.Tan(Math.PI / 180 * both.angle_extended_foot_taper);
+            curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[0], both.D_extended_top, localAxis[0]));
+            curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[1], both.D_extended_top, localAxis[0]));
+            curveList.Add(SectionCurve.CreateSolidColumnCircle(bothPos1, both.D_axial, localAxis[0]));
+            curveList.Add(SectionCurve.CreateSolidColumnCircle(bothPos3, both.D_axial, localAxis[0]));
+            curveList.Add(SectionCurve.CreateSolidColumnCircle(bothPos2, both.D_extended_foot, localAxis[0]));
+            curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[3], both.D_extended_foot, localAxis[0]));
+        }
+
+        private static void PileExtendedTop(IReadOnlyList<Point3d> sectionPoints, Vector3d axis, List<SectionCurve> curveList, Vector3d[] localAxis, double dExtend, double d, double angle)
+        {
+            var topPos1 = sectionPoints[1] + axis * (dExtend - d) / 2d / Math.Tan(Math.PI / 180 * angle);
+            curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[0], dExtend, localAxis[0]));
+            curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[1], dExtend, localAxis[0]));
+            curveList.Add(SectionCurve.CreateSolidColumnCircle(topPos1, d, localAxis[0]));
+            curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[3], d, localAxis[0]));
+        }
+
+        private static void PileExtendedFoot(IReadOnlyList<Point3d> sectionPoints, Vector3d axis, List<SectionCurve> curveList, Vector3d[] localAxis, double d, double dExtend, double length, double angle)
+        {
+            var footPos1 = sectionPoints[3] - axis * length;
+            var footPos2 = footPos1 - axis * (dExtend - d) / 2d / Math.Tan(Math.PI / 180 * angle);
+            curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[0], d, localAxis[0]));
+            curveList.Add(SectionCurve.CreateSolidColumnCircle(footPos2, d, localAxis[0]));
+            curveList.Add(SectionCurve.CreateSolidColumnCircle(footPos1, dExtend, localAxis[0]));
+            curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[3], dExtend, localAxis[0]));
+        }
+
+        private static void PileStraight(IReadOnlyList<Point3d> sectionPoints, List<SectionCurve> curveList, Vector3d[] localAxis, double d)
+        {
+            curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[0], d, localAxis[0]));
+            curveList.Add(SectionCurve.CreateSolidColumnCircle(sectionPoints[3], d, localAxis[0]));
         }
     }
 }
